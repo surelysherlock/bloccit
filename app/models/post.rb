4f668bae72ec4792a4f2a829a3a875cd4e0d4c49
Sanyan Sharar <raad34@gmail.com>
2017-10-11 11:16:40 -0400

    validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
     belongs_to :topic
     has_many :comments, dependent: :destroy
     has_many :sponsored_posts
+
+    validates :title, length: { minimum: 5 }, presence: true
+    validates :body, length: { minimum: 20 }, presence: true
+    validates :topic, presence: true
 end
