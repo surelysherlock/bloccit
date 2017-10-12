0c9e6f460528705c2694dd8720a29b6622da7cd0
Sanyan Sharar <raad34@gmail.com>
2017-10-12 10:51:28 -0400

    callbackcap

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
-    before_save { self.email = email.downcase if email.present? }
+    before_save { self.email = email.downcase if email.present?, 
+
+    currentArray = self.name.split
+    newArray = []
+    
+    for n in currentArray
+      newArray.push(n.capitalize)
+    end
+    
+    self.name = newArray.join(' ')
+     }
        validates :name, length: { minimum: 1, maximum: 100 }, presence: true
        validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
        validates :password, length: { minimum: 6 }, allow_blank: true
