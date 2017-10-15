dc5a67d86657ce8d27fc5b43d4cf1a83ef1b3ceb
Sanyan Sharar <raad34@gmail.com>
2017-10-15 10:43:36 -0400

    signin

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
+  resources :sessions, only: [:new, :create, :destroy]
+
   post 'users/confirm' => 'users#confirm'
 
   resources :advertisements
