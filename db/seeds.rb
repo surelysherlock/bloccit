6c8eb64d9d93fa8d27e69b09595a9f0abc850407
Sanyan Sharar <raad34@gmail.com>
2017-10-16 11:00:11 -0400

    posts&users

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,16 @@
 
 require 'random_data'
 
+# Create Users
+5.times do
+  User.create!(
+    name:     RandomData.random_name,
+    email:    RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+end
+users = User.all
+
  # Create Topics
   15.times do
     Topic.create!(
@@ -23,6 +33,7 @@ require 'random_data'
  50.times do
 
    Post.create!(
+     user:   users.sample,
      topic: topics.sample,
      title: RandomData.random_sentence,
      body: RandomData.random_paragraph
@@ -37,8 +48,15 @@ require 'random_data'
      body: RandomData.random_paragraph
    )
  end
+
+ user = User.first
+ user.update_attributes!(
+   email: 'raad34@gmail.com', 
+   password: 'helloworld'
+ )
  
  puts "Seed finished"
+ puts "#{User.count} users created"
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
+
+  it { is_expected.to have_many(:posts) }
+
   # tests for name
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
@@ -20,12 +23,6 @@ RSpec.describe User, type: :model do
   describe "attributes" do
     it "should have name and email attributes" do
       expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
-    end
-
-    it "should format the name" do
-      user.name = 'sanyan sharar'
-      user.save
-      expect(user.name).to eq("Sanyan Sharar")
     end
   end
 
