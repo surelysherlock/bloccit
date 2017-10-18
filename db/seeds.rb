95eeadeb47550edc044c73dc840071711650c8f0
Sanyan Sharar <raad34@gmail.com>
2017-10-17 22:31:00 -0400

    auth

--- db/seeds.rb
+++ db/seeds.rb
@@ -49,11 +49,20 @@ users = User.all
    )
  end
 
- user = User.first
- user.update_attributes!(
-   email: 'raad34@gmail.com', 
-   password: 'helloworld'
- )
+ # Create an admin user
+ admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+# Create a member
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld'
+)
  
  puts "Seed finished"
  puts "#{User.count} users created"
