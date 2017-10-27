6ac55af5b38e773e2724567eb8108aa96175c8ad
Sanyan Sharar <raad34@gmail.com>
2017-10-27 08:05:59 -0400

    addfunc

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,8 @@ module PostsHelper
   def user_is_authorized_for_post?(post)
     current_user &&  current_user.admin?
   end
+
+  def user_has_no_posts?
+    current_user.posts.count == 0
+  end
 end
